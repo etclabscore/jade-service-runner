6ae9d625089d9de00c66d5033f59c3689df2312a
Zane Starr <zcstarr@gmail.com>
2019-05-01 11:30:57 -0700

    fix(terrible): client generator format changed embedding full doc is always good idea fyi

--- index.ts
+++ index.ts
@@ -10,12 +10,16 @@ import { json as jsonParser } from "body-parser";
 import { promisify } from "util";
 import * as fs from "fs";
 const readFile = promisify(fs.readFile);
+import * as path from 'path';
+var appDir = path.resolve(__dirname);
 const client: ServiceRunner = new ServiceRunner({transport: {type:"http"}})
-//@ts-ignore
-const doc: OpenRPC= client["openrpcDocument"]
-const corsOptions = { origin: "*" } as cors.CorsOptions;
 
+  
+async function foo() {
 
+} 
+
+  const corsOptions = { origin: "*" } as cors.CorsOptions;
 
 const methodHandlerMapping: IMethodMapping = {
   installService: async (a, b) => {
@@ -28,6 +32,11 @@ const validateMethods = (method: string) => {
   if( typeof (client as any)[method] === "function") return  
   throw "Invalid method for router"
 }
+async function init(){
+  let doc: OpenRPC;
+  console.log(appDir + 'openrpc.json')
+  const data = await readFile(appDir + '/../openrpc.json')
+  doc = JSON.parse(data.toString());
 Object.keys(methodHandlerMapping).forEach(validateMethods)
 const router = new Router(doc, methodHandlerMapping);
 router.isMethodImplemented
@@ -46,7 +55,14 @@ const options = {
   openrpcDocument: doc 
 };
 
-      // @ts-ignore
+// @ts-ignore
 const server = new Server(options);
 
 server.start();
+}
+
+try {
+  init()
+} catch (e) {
+  console.log(e)
+}
