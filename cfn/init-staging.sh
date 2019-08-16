7d662ededb188bc8119829e71f26c4641cb78d9d
Zachary Belford <belfordz66@gmail.com>
2019-08-15 23:43:52 -0700

    fix: missing backslash

--- cfn/init-staging.sh
+++ cfn/init-staging.sh
@@ -5,7 +5,7 @@ aws cloudformation create-stack \
     --template-body file://cfn/jade-service-runner.cfn.json \
     --parameters file://cfn/staging-launch-params.json \
     --capabilities CAPABILITY_IAM \
-    --disable-rollback
+    --disable-rollback \
     --region us-west-2
 
 aws cloudformation wait stack-create-complete --stack-name jade-service-runner-staging-beanstalk-stack
