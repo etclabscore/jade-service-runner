55ffcc7bd34f78bee3d8ffcbd9e59256e9bbd92e
meows <b5c6@protonmail.com>
2020-03-20 10:53:37 -0500

    fix(core-geth examples): bump version from v1.9.0->v1.11.2
    These are just demos and comments, but it's nice
    to have them up-to-date, or at least
    to have them having been at least once upon a time
    working.
    
    Signed-off-by: meows <b5c6@protonmail.com>

--- client.ts
+++ client.ts
@@ -1,10 +1,10 @@
 /*const ServiceRunner = require('./build/client/typescript/build/index').default;
 const client = new ServiceRunner({ transport: { type: "http", port: 8002, host: "localhost" } });
-client.installService("core-geth", "1.9.0")
+client.installService("core-geth", "1.11.2")
   .then(() => client.listInstalledServices())
   .then(() => client.listRunningServices())
   .then(console.log)
-  .then(() => client.startService("core-geth", "1.9.0", "mainnet"))
+  .then(() => client.startService("core-geth", "1.11.2", "mainnet"))
   .then(console.log)
   .then(() => client.listRunningServices())
   .then(console.log)
