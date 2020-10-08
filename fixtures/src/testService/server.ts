3a7497d4cb063bfda20e785392b6d712565eca4d
Zachary Belford <belfordz66@gmail.com>
2020-10-07 17:10:01 -0700

    fix: meta-schema exports are different now

--- fixtures/src/testService/server.ts
+++ fixtures/src/testService/server.ts
@@ -1,10 +1,10 @@
-import { OpenRPC } from "@open-rpc/meta-schema";
-import { Server, IServerOptions } from "@open-rpc/server-js";
+import { OpenrpcDocument as OpenRPC } from "@open-rpc/meta-schema";
+import { Server, ServerOptions } from "@open-rpc/server-js";
 export type TestProtocol = "ws" | "http";
 
 export const testServer = (port: number, protocol: TestProtocol, openrpcDocument: OpenRPC, opts: any) => {
 
-  let options = {} as IServerOptions;
+  let options = {} as ServerOptions;
 
   let transportConfigs;
   switch ( protocol) {
