8dd8f76206876be28a1badc5385151134a7c7a3f
shanejonas <jonas.shane@gmail.com>
2020-11-06 19:12:52 -0800

    fix: use new generator

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/jade-service-runner/build/generated-client/typescript/.npmrc
-cd build/generated-client/typescript && npm install && npm run build && npm publish --access=public
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/jade-service-runner/build/generated-client/client/typescript/.npmrc
+cd build/generated-client/client/typescript && npm install && npm run build && npm publish --access=public
